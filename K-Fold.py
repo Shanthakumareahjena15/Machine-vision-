3e34d649921e270987d6d7cbb077cb4605902a7a
Shanthakumareahjena15 <32158273+Shanthakumareahjena15@users.noreply.github.com>
2020-10-02 12:41:25 +0200

    Update K-Fold.py

--- K-Fold.py
+++ K-Fold.py
@@ -1,2 +1,5 @@
 from sklearn.model_selection import StratifiedKFold
 import numpy as np
+
+#train_image, train_label = data_preparation.data(base_dir ='D:\Randomforest',  image_dir = '/Trainexample1/*')
+#test_image, test_label = data_preparation.data(base_dir ='D:\Randomforest',  image_dir = '/Validation-Example1/*')

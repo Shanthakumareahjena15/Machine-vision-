354934b3a49c7ed5ccdcc15d475204a9c21ce4bd
ShanthaKumarR <32158273+ShanthaKumarR@users.noreply.github.com>
2020-10-02 15:01:22 +0200

    Update data_preparation.py

--- data_preparation.py
+++ data_preparation.py
@@ -8,3 +8,13 @@ from keras.applications.inception_v3 import preprocess_input, decode_predictions
 from keras.layers import Input, Dense, Dropout, GlobalAveragePooling2D
 from keras.models import Model
 from keras.applications.vgg16 import VGG16
+
+class data_preparation:
+  def __init__(self):
+    pass
+  def image_to_array():
+    image = []
+    lables = []
+    for directory_path in glob.glob(base_dir):
+      
+    

9b3270124c8835d6be675494115cdb26a84d7961
ShanthaKumarR <32158273+ShanthaKumarR@users.noreply.github.com>
2020-10-02 15:16:18 +0200

    Update data_preparation.py

--- data_preparation.py
+++ data_preparation.py
@@ -10,11 +10,22 @@ from keras.models import Model
 from keras.applications.vgg16 import VGG16
 
 class data_preparation:
-  def __init__(self):
-    pass
-  def image_to_array():
-    image = []
-    lables = []
-    for directory_path in glob.glob(base_dir):
-      
+    def __init__(self, base, image_dir):
+        self.base = base
+        self.image_dir = image_dir
+    
+    def image_to_array(self):
+        image = []
+        lable = []
+        for directory_path in glob.glob(self.base + self.image_dir):
+            folder_name = directory_path.split("\\")[-1]   
+            print(directory_path)             
+            for img_path in glob.glob(os.path.join(directory_path, '*.jpg')):
+                img = cv2.imread(img_path, cv2.IMREAD_COLOR) 
+                img = cv2.resize(img, (224, 224))
+                img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)            
+                image.append(img)
+                lable.append(folder_name)   
+        return np.array(image), np.array(lable)
+    
     

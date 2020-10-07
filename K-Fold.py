312c3c5e5ff7bc40b547b1e2b49787fcec4a3641
ShanthaKumarR <32158273+ShanthaKumarR@users.noreply.github.com>
2020-10-07 13:44:04 +0200

    Update K-Fold.py

--- K-Fold.py
+++ K-Fold.py
@@ -1,5 +1,44 @@
+import pandas as pd
 from sklearn.model_selection import StratifiedKFold
 import numpy as np
 
-#train_image, train_label = data_preparation.data(base_dir ='D:\Randomforest',  image_dir = '/Trainexample1/*')
-#test_image, test_label = data_preparation.data(base_dir ='D:\Randomforest',  image_dir = '/Validation-Example1/*')
+train_features_pandas = pd.read_csv('cod_her_train_xg.csv')
+test_features_pandas = pd.read_csv('cod_her_test_xg.csv')
+train_labels = pd.read_csv('train_label.csv')
+test_labels = pd.read_csv('test_label.csv')
+
+
+class k_fold:
+  def_init__(self):
+    pass
+  def index_split(train_features_pandas, train_label_ids):
+    i = 0
+     for train_index, test_index in skf.split(train_features_pandas, train_label_ids):
+        if i == 0:
+            train_index_1 = train_index 
+            test_index_1 = test_index
+        elif i == 1:
+            train_index_2 = train_index 
+            test_index_2 = test_index
+        elif i == 2:
+            train_index_3 = train_index
+            test_index_3 = test_index
+        
+        i += 1
+     return train_index_1, test_index_1, train_index_2, test_index_2, train_index_3, test_index_3
+  
+  def k_fold_data_seperation(train_index_1, train_index_2, train_index_3):        
+    train_data_one = []
+    train_data_two = []
+    train_data_three = []
+    for i in train_index_1:
+        train_data_one.append(train_data_array[i])
+    for i in train_index_2:
+        train_data_two.append(train_data_array[i])
+    for i in train_index_3:
+        train_data_three.append(train_data_array[i])
+    train_data_one_array = np.array(train_data_one)
+    train_data_two_array = np.array(train_data_two)
+    train_data_three_array = np.array(train_data_three)
+    
+    return train_data_one_array, train_data_two_array, train_data_three_array

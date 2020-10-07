7fd9ffb2ec951eb0790ced1b493d8916f9d61a76
ShanthaKumarR <32158273+ShanthaKumarR@users.noreply.github.com>
2020-10-07 16:50:58 +0200

    Update K-Fold.py

--- K-Fold.py
+++ K-Fold.py
@@ -2,6 +2,9 @@ import pandas as pd
 from sklearn.model_selection import StratifiedKFold
 import numpy as np
 
+skf = StratifiedKFold(n_splits=3,  random_state=None, shuffle=True)
+skf.get_n_splits(train_data_array, train_label_ids)
+
 train_features_pandas = pd.read_csv('cod_her_train_xg.csv')
 test_features_pandas = pd.read_csv('cod_her_test_xg.csv')
 train_labels = pd.read_csv('train_label.csv')

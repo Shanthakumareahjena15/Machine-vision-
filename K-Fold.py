01efce3ef24757384d7e4b70d8f0865aa5c1cd7b
ShanthaKumarR <32158273+ShanthaKumarR@users.noreply.github.com>
2020-10-28 16:36:13 +0100

    Update K-Fold.py

--- K-Fold.py
+++ K-Fold.py
@@ -45,3 +45,45 @@ class k_fold:
     train_data_three_array = np.array(train_data_three)
     
     return train_data_one_array, train_data_two_array, train_data_three_array
+  
+  
+  def k_fold_data_seperation(test_index_1, test_index_2, test_index_3):        
+    test_data_one = []
+    test_data_two = []
+    test_data_three = []
+    for i in test_index_1:
+        test_data_one.append(train_data_array[i])
+    for i in test_index_2:
+        test_data_two.append(train_data_array[i])
+    for i in test_index_3:
+        test_data_three.append(train_data_array[i])
+    test_data_one_array = np.array(test_data_one)
+    test_data_two_array = np.array(test_data_two)
+    test_data_three_array = np.array(test_data_three)
+    
+    return test_data_one_array, test_data_two_array, test_data_three_array
+
+
+  def train_label_index(train_label_ids):
+    train_label_index_1 = []
+    train_label_index_2 = []
+    train_label_index_3 = []
+    for i in train_index_1:
+        train_label_index_1.append(train_label_ids[i])
+    for i in train_index_2:
+        train_label_index_2.append(train_label_ids[i])
+    for i in train_index_3:
+        train_label_index_3.append(train_label_ids[i])
+    train_label_index_1 = pd.DataFrame(train_label_index_1) 
+    train_label_index_1.to_csv( 'train_label_id_1.csv', index = False)
+    train_label_index_2 = pd.DataFrame(train_label_index_2)
+    train_label_index_2.to_csv('train_label_id_2.csv', index = False)
+    train_label_index_3 =pd.DataFrame(train_label_index_3)
+    train_label_index_3.to_csv('train_label_id_3.csv', index = False)
+    
+    return train_label_index_1, train_label_index_2, train_label_index_3
+
+
+
+
+

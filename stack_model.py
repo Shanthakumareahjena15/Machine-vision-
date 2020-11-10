daf4fea6363602042c1b5720254071ad50a3000d
ShanthaKumarR <32158273+ShanthaKumarR@users.noreply.github.com>
2020-11-10 17:21:43 +0100

    Update stack_model.py

--- stack_model.py
+++ stack_model.py
@@ -54,3 +54,61 @@ class BaseModels:
         rf_oneHot_predict = rf_model_one_hot.predict(test_fetrures)
         rf_predictproba = rf_model_proba.predict_proba(test_features)
         return rf_oneHot_predict, rf_predictproba
+    
+    
+    class MetaTraining_data:
+    
+    def __init__(self, test_label_index1, test_label_index2, test_label_index3):
+        self.test_label_index1 = test_label_index1
+        self.test_label_index2 = test_label_index2
+        self.test_label_index3 = test_label_index3
+    
+    def meta_training_data(self, test_pred_1, test_pred_2, test_pred_3 ):  
+        self.test_pred_1 = test_pred_1
+        self.test_pred_2 = test_pred_2
+        self.test_pred_3 = test_pred_3
+        prediction = np.zeros((1117, 4))
+        for i, j in zip (test_pred_1, np.array(test_label_index1)):
+           prediction[j] = i
+           
+        for i, j in zip (test_pred_2, np.array(test_label_index2)):
+           prediction[j] = i
+        
+        for i, j in zip (test_pred_3, np.array(test_label_index3)):
+           prediction[j] = i
+           
+        return prediction
+    
+  
+    def Concatenate_testData(self, lr_prediction, ab_prediction, rf_prediction):
+        self.lr_prediction = lr_prediction
+        self.ab_prediction = ab_prediction
+        self.rf_prediction = rf_prediction
+        concatenated_data =  np.concatenate(( lr_prediction, ab_prediction, rf_prediction), axis=1)
+        
+        return concatenated_data
+
+
+
+
+class Metamodel:
+    def __init__(self):
+        pass
+
+        
+        def MetaTraining_data():
+            
+        
+        def XGBoost_predictProba(self, concatenated_data):  
+            self.concatenated_data = concatenated_data
+            params = {'max_depth':3, 'eta':0.01,'silent':1,  'num_class':4,'objective':'multi:softprob' } 
+            meta_training_data_data =  xgboost.DMatrix(concatenated_data, label_ids)
+            model_xg = xgboost.train(params, dataset, num_boost_round=100)           
+            return model_xg 
+        def XGBoost_predict(self):
+             meta_testing_data =  xgboost.DMatrix(np.concatenate(( lr_prediction, ab_prediction, rf_prediction), axis=1))
+             params = {'max_depth':3, 'eta':0.01,'silent':1,  'num_class':4,'objective':'multi:softmax' } 
+             dataset = xgboost.DMatrix(meta_training_data, labels )
+             model_xg = xgboost.train(params, dataset, num_boost_round=100)
+             xg_train_one_hot = model_xg.predict(dataset)
+

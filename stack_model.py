a14efcbdd8afc7b76d4c3e08ac3b916addab12d9
ShanthaKumarR <32158273+ShanthaKumarR@users.noreply.github.com>
2020-11-20 16:20:36 +0100

    Update stack_model.py

--- stack_model.py
+++ stack_model.py
@@ -7,6 +7,7 @@ from sklearn.ensemble import RandomForestClassifier
 import xgboost
 from xgboost import XGBClassifier
 import pickle 
+import os 
 
 train_features = pd.read_csv('training_feature.csv')
 train_labels = pd.read_csv('training_label.csv')
@@ -50,7 +51,7 @@ class BaseModels:
     def __init__(self):
         pass
     
-    def logistic_regression(self, features, labels, test_features, file_name):
+    def logistic_regression(self, features, labels, test_features, file_name = None):
         self.features = features
         self.labels = labels
         #self.label_ids = label_ids
@@ -70,7 +71,7 @@ class BaseModels:
             pickle_out.close()
            # return lr_model_proba
         
-    def ada_boost(self, features, labels,  test_features, file_name):
+    def ada_boost(self, features, labels,  test_features, file_name = None):
         self.features = features
         self.labels = labels
         self.test_features = test_features
@@ -88,7 +89,7 @@ class BaseModels:
             pickle_out.close()
             #return adaBoost_model_proba
     
-    def Randomforest(self, features, labels, test_features, file_name):
+    def Randomforest(self, features, labels, test_features, file_name = None):
         self.features = features
         self.labels = labels
         self.test_features = test_features
@@ -158,19 +159,18 @@ class Metamodel:
         meta_training_data_data =  xgboost.DMatrix(concatenated_data, label)
         model_xg = xgboost.train(params, meta_training_data_data, num_boost_round=100)
         pickle_out = open(file_name, 'wb')
-        pickle.dump(lr_model_proba, pickle_out)
+        pickle.dump(model_xg, pickle_out)
         pickle_out.close()
         return model_xg 
     
-    def XGBoost_predict(self, concatenated_data, file_name):
+    def XGBoost_predict(self, concatenated_data, label, file_name):
          self.concatenated_data = concatenated_data
          self.file_name = file_name
-         meta_testing_data =  xgboost.DMatrix(np.concatenate(( lr_prediction, ab_prediction, rf_prediction), axis=1))
          params = {'max_depth':3, 'eta':0.01,'silent':1,  'num_class':4,'objective':'multi:softmax' } 
-         dataset = xgboost.DMatrix(concatenated_data, labels )
+         dataset = xgboost.DMatrix(concatenated_data, label )
          model_xg = xgboost.train(params, dataset, num_boost_round=100)
          pickle_out = open(file_name, 'wb')
-         pickle.dump(lr_model_proba, pickle_out)
+         pickle.dump(model_xg, pickle_out)
          pickle_out.close()
          
          return model_xg
@@ -183,19 +183,19 @@ predectionLabelRf_1, predectionProbaRf_1 = layer_1A.Randomforest(features = trai
 predectionLabelRf_2, predectionProbaRf_2 = layer_1A.Randomforest(features = train_2, labels = trainLabel_2, test_features = test_2)
 predectionLabelRf_3, predectionProbaRf_3 = layer_1A.Randomforest(features = train_3, labels = trainLabel_3, test_features = test_3)
 
-layer_1A.Randomforest(features = train_features, labels = train_labels, test_features = None, file_name = 'D:/Randomforest/figs/randomforest_b1.pickle')
+layer_1A.Randomforest(features = train_features, labels = train_labels, test_features = None, file_name = os.getcwd() + '/YOLO/randomforest_b1.pickle')
 
 predectionLabelab_1, predectionProbaab_1 = layer_1A.ada_boost(features = train_1, labels = trainLabel_1, test_features = test_1)
 predectionLabelab_2, predectionProbaab_2 = layer_1A.ada_boost(features = train_2, labels = trainLabel_2, test_features = test_2)
 predectionLabelab_3, predectionProbaab_3 = layer_1A.ada_boost(features = train_3, labels = trainLabel_3, test_features = test_3)
 
-layer_1A.ada_boost(features = train_features, labels = train_labels, test_features = None, file_name = 'abaBoost_b2.pickle')
+layer_1A.ada_boost(features = train_features, labels = train_labels, test_features = None, file_name = os.getcwd() + '/YOLO/abaBoost_b2.pickle')
 
 predectionLabelLr_1, predectionProbaLr_1 = layer_1A.logistic_regression(features = train_1, labels = trainLabel_1, test_features = test_1)
 predectionLabelLr_2, predectionProbaLr_2 = layer_1A.logistic_regression(features = train_2, labels = trainLabel_2, test_features = test_2)
 predectionLabelLr_3, predectionProbaLr_3 = layer_1A.logistic_regression(features = train_3, labels = trainLabel_3, test_features = test_3)
 
-layer_1A.logistic_regression(features = train_features, labels = train, test_features = None, file_name = 'LogisticRegression_b3')
+layer_1A.logistic_regression(features = train_features, labels = train_labels, test_features = None, file_name = os.getcwd() + '/YOLO/LogisticRegression_b3')
 
 rf_dat_1 = metaTrainingData.meta_training_data(predectionProbaRf_1, predectionProbaRf_2, predectionProbaRf_3)
 rf_dat_2 = metaTrainingData.meta_training_data(predectionProbaab_1, predectionProbaab_2, predectionProbaab_3)
@@ -203,4 +203,4 @@ rf_dat_3 = metaTrainingData.meta_training_data(predectionLabelLr_1, predectionLa
 
 con_data = metaTrainingData.Concatenate_testData(rf_dat_1, rf_dat_2, rf_dat_3)
 
-model_xg = meta_xg.XGBoost_predict(con_data, train_labelIds, file_name = 'metaclassifier.dat')
+model_xg = meta_xg.XGBoost_predict(con_data, train_labelIds, file_name = os.getcwd() + '/results/metaclassifier.dat')

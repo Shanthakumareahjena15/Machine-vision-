583d273b548a5ea0cb3bb130bd5b79163837a5cb
ShanthaKumarR <32158273+ShanthaKumarR@users.noreply.github.com>
2020-11-23 17:05:01 +0100

    Delete count.py

--- YOLO/yolov3_tf2/count.py
+++
@@ -1,8 +0,0 @@
-
-import importlib.machinery
-
-
-loader = importlib.machinery.SourceFileLoader('Object-Detection-API-master', 'D:\yolo_v3_object_detection\Object-Detection-API-master\module_test.py')
-handle = loader.load_module('module_test.py')
-
-#handle.module_test()
